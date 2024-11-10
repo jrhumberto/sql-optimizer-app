7ce7501f5f2042f5f0b5fe152142cc8d1542ab19
Humberto BM Junior <64866505+jrhumberto@users.noreply.github.com>
2024-11-10 01:41:44 -0300

    Commit inicial

--- app.py
+++ app.py
@@ -3,7 +3,37 @@ import os
 import streamlit as st
 from streamlit_ace import st_ace
 
-from util import RULE_MAPPING, SAMPLE_QUERY, apply_optimizations, format_sql_with_sqlfmt
+
+from typing import Callable, Dict, Sequence
+from sqlglot import parse_one
+from sqlglot.expressions import Select
+from sqlglot.optimizer import RULES, optimize
+from sqlfmt.api import Mode, format_string
+
+RULE_MAPPING: Dict[str, Callable] = {rule.__name__: rule for rule in RULES}
+
+SAMPLE_QUERY: str = """WITH users AS (
+    SELECT *
+    FROM users_table),
+orders AS (
+    SELECT *
+    FROM orders_table),
+combined AS (
+    SELECT users.id, users.name, orders.order_id, orders.total
+    FROM users
+    JOIN orders ON users.id = orders.user_id)
+SELECT combined.id, combined.name, combined.order_id, combined.total
+FROM combined
+"""
+
+
+def apply_optimizations(query: str, rules: Sequence[Callable] = RULES, remove_ctes: bool = False) -> Select:
+    return optimize(ast=parse_one(query) , rules=rules, leave_tables_isolated=not(remove_ctes))
+
+
+def format_sql_with_sqlfmt(query: str) -> str:
+    return format_string(query, mode = Mode())
+
 
 st.set_page_config(layout="wide")
 
